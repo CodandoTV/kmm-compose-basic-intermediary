c88199f16f5f26acf90ac6f8d48d9381a00f1a2d
git-jr <junior.carvalho@angellira.com.br>
2024-12-21 11:16:39 -0300

    fix: IOS missing dependence

--- composeApp/src/iosMain/kotlin/createDataStore.ios.kt
+++ composeApp/src/iosMain/kotlin/createDataStore.ios.kt
@@ -1,7 +1,14 @@
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import data.dataStoreFileName
+import platform.Foundation.NSURL
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSUserDomainMask
+import data.createDataStore
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> = createDataStore(
     producePath = {
         val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
