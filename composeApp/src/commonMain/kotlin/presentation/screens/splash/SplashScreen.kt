8a447651a04ed75de6ec25b8ee0815b95aa6fa18
git-jr <junior.carvalho@angellira.com.br>
2024-12-21 10:14:18 -0300

    feat: state on SplashScreen

--- composeApp/src/commonMain/kotlin/presentation/screens/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/splash/SplashScreen.kt
@@ -8,18 +8,34 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import org.koin.compose.viewmodel.koinViewModel
+import presentation.NavigationRoutes
 
 @Composable
 fun SplashScreen(
     navController: NavController,
     viewModel: SplashViewModel = koinViewModel()
 ) {
+
+    val state by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(state.appState){
+        if (state.appState == AppState.NotLoggedIn) {
+            navController.navigate(NavigationRoutes.Login)
+        }
+
+        if (state.appState == AppState.LoggedIn) {
+            navController.navigate(NavigationRoutes.ForgotPassword)
+        }
+    }
 
     LoadScreen()
 }
--- composeApp/src/commonMain/kotlin/presentation/screens/splash/SplashViewModel.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/splash/SplashViewModel.kt
@@ -1,13 +1,34 @@
 package presentation.screens.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import data.LoginRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import presentation.screens.login.LoginUIState
 
+data class SlashUiState(
+    val appState: AppState = AppState.Loading
+)
+
+
+sealed class AppState {
+    data object Loading : AppState()
+    data object LoggedIn : AppState()
+    data object NotLoggedIn : AppState()
+}
+
 class SplashViewModel() : ViewModel() {
-//    private val _uiState = MutableStateFlow(LoginUIState())
-//    val uiState: StateFlow<LoginUIState>
-//        get() = _uiState
+    private val _uiState = MutableStateFlow(SlashUiState())
+    val uiState: StateFlow<SlashUiState>
+        get() = _uiState
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _uiState.value = _uiState.value.copy(appState = AppState.LoggedIn)
+        }
+    }
 }
